457f44100b2fab816803e2113911b6545b400b2a
AndreJesusBrito <andrejesusdebrito@gmail.com>
2018-11-21 15:24:54 +0000

    fancify MEmes

---
+++ users.sql
@@ -0,0 +1,181 @@
+CREATE TABLE users(
+    ID INT Primary key,
+    name VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255) unique,
+    birthday DATE,
+    bio VARCHAR(2000)
+);
+
+insert into users values(
+    '0000001',
+    'Jesus',
+    '******',
+    'mormonsareright@believe.faith',
+    '1/1/0000',
+    'DEUS VULT!'
+);
+
+insert into users values(
+    '0000002',
+    'Hamid coracao de bufalo',
+    '******',
+    'hamid@mosquitos.busy',
+    '2/1/0000',
+    'o programa funciona??? final entao'
+);
+
+
+insert into users values(
+    '02133331',
+    'Sir John Smith Smith	',
+    '******',
+    'gentlmen@sir.uk',
+    '11/12/1965',
+    'part time Connoisseur'
+);
+
+
+insert into users values(
+    '96133796',
+    'Kid Ninja',
+    '******',
+    'fornite@victoryroyal.epicgames',
+    '7/10/2005',
+    'I LOVE FORTNITE'
+);
+
+
+insert into users values(
+    '293222332',
+    'Miley Cyrus',
+    '******',
+    'mileycyrus@wreckingball.wreck',
+    '7/10/1995',
+    'WRECKING'
+);
+
+insert into users values(
+    '120032222',
+    'Andre de gambelas',
+    '******',
+    'andredegambelas@gambelas.andre',
+    '1/02/1999',
+    'Ola eu sou o diogo'
+);
+
+insert into users values(
+    '002222333',
+    'Bruno de fatima',
+    '******',
+    'brunobruno@pray.jesus',
+    '10/12/1995',
+    '#faith+1'
+);
+
+
+insert into users values(
+    '21112221',
+    'Teresa de almancil',
+    '******',
+    'eggswithoutyolk@eggs.chicken',
+    '6/11/1999',
+    'comigo'
+);
+
+
+insert into users values(
+    '83222322',
+    'Leandro de Olhao',
+    '******',
+    'facadas@stabby.streetcorner',
+    '23/8/1999',
+    'give me your money'
+);
+
+
+insert into users values(
+    '83222321',
+    'Dario de olhao',
+    '******',
+    'facadelas@stabby.streetcorner',
+    '23/5/1999',
+    'give me your pizza'
+);
+
+
+insert into users values(
+    '13371337',
+    'Berna Weezerd de Sines',
+    '******',
+    'houdini@nowyouseeme.nowyoudont',
+    '10/2/1999',
+    'imagine living in 2018 and not do magic lmaooo'
+);
+
+insert into users values(
+    '82331222',
+    'Tiago Nunes de Lisboa',
+    '******',
+    'nunestiago@pediuguitarra.denied',
+    '12/3/1999',
+    'eu falo mais alto que a televis�o'
+);
+
+insert into users values(
+    '31432791',
+    'Ricardo-chan de Almancil/loule place kawaiiiuu',
+    '******',
+    'revoliatneh@weeb.desu',
+    '7/8/2008',
+    'I LOVE ANIME!!!!'
+);
+
+
+insert into users values(
+    '42134929',
+    'Michael Jackson',
+    '******',
+    'shamona@tehee.shamona',
+    '25/06/2009',
+    'ARE YOU OKAY ANI???'
+);
+
+
+insert into users values(
+    '84798479',
+    'Toto africa de africa',
+    '******',
+    'toto@at.africa',
+    '7/11/1008',
+    'gonna take some time to do the things we never had'
+);
+
+
+insert into users values(
+    '92187421',
+    'Erique',
+    '******',
+    'erique@ir.quadro',
+    '7/8/1882',
+    'born,suffer,death (notice that dead is in past, cause im already dead)'
+);
+
+
+insert into users values(
+    '1231232',
+    'Guilherme matrix matrix',
+    '******',
+    'matrix@hacker.mrrobot',
+    '31/12/1999',
+    'mooshak <3'
+);
+
+insert into users values {
+    '1231231',
+    'Mark Zuckerberg',
+    '******',
+    'markZuckerberg@facebook.com',
+    '14/05/1984',
+    'This network its much better than Facebook, indeed'
+}
