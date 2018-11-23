4e84d93c82dba63068fc10c8138ce6fbb0316f63
cptSagres <39467476+cptSagres@users.noreply.github.com>
2018-11-23 16:52:55 +0000

    Add files via upload

---
+++ users50.sql
@@ -0,0 +1,500 @@
+insert into users values(
+	'0',
+	'CHRISTOPHER KING',
+	'Berlin',
+	'Germany',
+	'CHRISTOPHERKING@gmail.com',
+	'bfmjvxiz',
+	'15/1/1985',
+	'Sofrer, eh so uma vez; vencer, eh para a eternidade.');
+
+insert into users values(
+	'1',
+	'JUDITH GALLEGOS',
+	'Jakarta',
+	'Indonesia',
+	'JUDITHGALLEGOS@outlook.com',
+	'pbiokimj',
+	'2/9/1928',
+	'Lutar contra a injustica custa me mais do que sofre la.');
+
+insert into users values(
+	'2',
+	'CARRIE HOPPER',
+	'Berlin',
+	'Germany',
+	'CARRIEHOPPER@hotmail.com',
+	'wwlciavk',
+	'10/9/1942',
+	'So eh lutador quem sabe lutar consigo mesmo.');
+
+insert into users values(
+	'3',
+	'BRIAN LEVINE',
+	'Chennai',
+	'India',
+	'BRIANLEVINE@ualg.pt',
+	'sdvazztn',
+	'26/3/1975',
+	'Vale mais lutar com gente de bem do que triunfar sobre gente ruim.');
+
+insert into users values(
+	'4',
+	'ALLAN VASQUEZ',
+	'Moscow',
+	'Russia',
+	'ALLANVASQUEZ@hotmail.com',
+	'rumpvjhy',
+	'13/10/1977',
+	'Perder para a razao, sempre eh ganhar.');
+
+insert into users values(
+	'5',
+	'BRANDON JOHNSTON',
+	'Harbin',
+	'China',
+	'BRANDONJOHNSTON@ualg.pt',
+	'jgjctjsa',
+	'23/2/1920',
+	'Sonha e seras livre de espirito... luta e seras livre na vida.');
+
+insert into users values(
+	'6',
+	'MARIE SCHROEDER',
+	'Singapore',
+	'Singapore',
+	'MARIESCHROEDER@hotmail.com',
+	'logjsvme',
+	'9/4/2001',
+	'Eh mais dificil vencer habitos ruins do que despedacar rochas.');
+
+insert into users values(
+	'7',
+	'ANTONIO BUCK',
+	'Saint_Petersburg',
+	'Russia',
+	'ANTONIOBUCK@gmail.com',
+	'bscrbxdn',
+	'10/1/1923',
+	'O primeiro grau do heroismo eh vencer o medo.');
+
+insert into users values(
+	'8',
+	'ROGER WILKINSON',
+	'Xiamen',
+	'China',
+	'ROGERWILKINSON@gmail.com',
+	'lpuwgyhp',
+	'14/11/2000',
+	'Sofrer, eh so uma vez; vencer, eh para a eternidade.');
+
+insert into users values(
+	'9',
+	'SARA AGUILAR',
+	'Cape_Town',
+	'South',
+	'SARAAGUILAR@ualg.pt',
+	'fhhpcgtg',
+	'19/1/1993',
+	'A arte de vencer se aprende nas derrotas.');
+
+insert into users values(
+	'10',
+	'MAUREEN CERVANTES',
+	'Ankara',
+	'Turkey',
+	'MAUREENCERVANTES@gmail.com',
+	'tszgbeib',
+	'12/1/1965',
+	'O homem digno ganha para viver; o menos honesto vive para ganhar.');
+
+insert into users values(
+	'11',
+	'TODD GILMORE',
+	'Casablanca',
+	'Morocco',
+	'TODDGILMORE@hotmail.com',
+	'blfnggsu',
+	'26/11/1998',
+	'Vale mais lutar com gente de bem do que triunfar sobre gente ruim.');
+
+insert into users values(
+	'12',
+	'BERNARD PRUITT',
+	'Ekurhuleni',
+	'South',
+	'BERNARDPRUITT@hotmail.com',
+	'tjyafeja',
+	'27/3/1994',
+	'Lutar contra a injustica custa me mais do que sofre la.');
+
+insert into users values(
+	'13',
+	'MIKE PATTON',
+	'Jeddah',
+	'Saudi_Arabia',
+	'MIKEPATTON@gmail.com',
+	'ykmivdqb',
+	'8/1/1961',
+	'O homem nasceu para lutar e a sua vida eh uma eterna batalha.');
+
+insert into users values(
+	'14',
+	'RANDY NIEVES',
+	'Hefei',
+	'China',
+	'RANDYNIEVES@hotmail.com',
+	'farblorg',
+	'15/1/1942',
+	'O homem nasceu para lutar e a sua vida eh uma eterna batalha.');
+
+insert into users values(
+	'15',
+	'NINA MCFADDEN',
+	'Casablanca',
+	'Morocco',
+	'NINAMCFADDEN@outlook.com',
+	'abitmcfc',
+	'12/8/2014',
+	'So se pode vencer a natureza obedecendo lhe.');
+
+insert into users values(
+	'16',
+	'JOHNNIE RHODES',
+	'Karachi',
+	'Pakistan',
+	'JOHNNIERHODES@hotmail.com',
+	'psludxxh',
+	'7/11/1947',
+	'Nao ha dor que o sono nao consiga vencer.');
+
+insert into users values(
+	'17',
+	'CLARA DAVENPORT',
+	'Foshan',
+	'China',
+	'CLARADAVENPORT@ualg.pt',
+	'mnnqqzhi',
+	'22/9/1955',
+	'O homem nasceu para lutar e a sua vida eh uma eterna batalha.');
+
+insert into users values(
+	'18',
+	'EDNA BOONE',
+	'Busan',
+	'South',
+	'EDNABOONE@ualg.pt',
+	'ltdcposr',
+	'24/8/2007',
+	'Aquele que nao tem confianca nos outros, nao lhes pode ganhar a confianca.');
+
+insert into users values(
+	'19',
+	'DAVE SIMS',
+	'Tianjin',
+	'China',
+	'DAVESIMS@hotmail.com',
+	'tbuattcy',
+	'10/6/1994',
+	'Eh facil adquirir um inimigo; dificil eh conquistar um amigo.');
+
+insert into users values(
+	'20',
+	'LINDA TORRES',
+	'Kinshasa',
+	'Democratic_Republic_of_the_Congo',
+	'LINDATORRES@hotmail.com',
+	'gjamwyuw',
+	'7/7/1965',
+	'Prefiro perder a guerra e ganhar a paz.');
+
+insert into users values(
+	'21',
+	'HEIDI CARSON',
+	'Delhi',
+	'India',
+	'HEIDICARSON@hotmail.com',
+	'ceojqglt',
+	'13/7/1931',
+	'Viver significa lutar.');
+
+insert into users values(
+	'22',
+	'KELLY AVILA',
+	'Seoul',
+	'South_Korea',
+	'KELLYAVILA@ualg.pt',
+	'ccnirdye',
+	'4/11/1942',
+	'Eh mais facil vencer um mau habito hoje do que amanha.');
+
+insert into users values(
+	'23',
+	'BRENT JARVIS',
+	'Hanoi',
+	'Vietnam',
+	'BRENTJARVIS@hotmail.com',
+	'whoplufm',
+	'8/4/1961',
+	'O homem nasceu para lutar e a sua vida eh uma eterna batalha.');
+
+insert into users values(
+	'24',
+	'RUTH ACOSTA',
+	'Faisalabad',
+	'Pakistan',
+	'RUTHACOSTA@gmail.com',
+	'rgbpzdhc',
+	'18/7/1998',
+	'So ha uma maneira de lutar contra o poder: eh sobreviver lhe.');
+
+insert into users values(
+	'25',
+	'RONALD MCGUIRE',
+	'Bangalore',
+	'India',
+	'RONALDMCGUIRE@ualg.pt',
+	'drkkizgg',
+	'2/2/1989',
+	'Nao ha dor que o sono nao consiga vencer.');
+
+insert into users values(
+	'26',
+	'REBECCA MORGAN',
+	'Riyadh',
+	'Saudi_Arabia',
+	'REBECCAMORGAN@outlook.com',
+	'hesoqtwq',
+	'16/6/1940',
+	'Perder para a razao, sempre eh ganhar.');
+
+insert into users values(
+	'27',
+	'DANIEL STUART',
+	'Madrid',
+	'Spain',
+	'DANIELSTUART@gmail.com',
+	'hsayuawg',
+	'1/5/1926',
+	'De que serve ao homem conquistar o mundo inteiro se perder a alma?');
+
+insert into users values(
+	'28',
+	'LEWIS CASH',
+	'Berlin',
+	'Germany',
+	'LEWISCASH@gmail.com',
+	'qqbkbxbs',
+	'13/9/1944',
+	'O primeiro grau do heroismo eh vencer o medo.');
+
+insert into users values(
+	'29',
+	'STEVEN HODGE',
+	'Suzhou',
+	'China',
+	'STEVENHODGE@hotmail.com',
+	'ibjgfhzl',
+	'24/5/2002',
+	'So ha uma maneira de lutar contra o poder: eh sobreviver lhe.');
+
+insert into users values(
+	'30',
+	'BRIAN ZAMORA',
+	'Addis_Ababa',
+	'Ethiopia',
+	'BRIANZAMORA@hotmail.com',
+	'bwlduhsf',
+	'4/3/1934',
+	'Sofrer, eh so uma vez; vencer, eh para a eternidade.');
+
+insert into users values(
+	'31',
+	'MARC GAMBLE',
+	'Wuhan',
+	'China',
+	'MARCGAMBLE@outlook.com',
+	'iutnfpbw',
+	'25/2/1957',
+	'O medo de perder tira a vontade de ganhar.');
+
+insert into users values(
+	'32',
+	'CARRIE WATTS',
+	'Chennai',
+	'India',
+	'CARRIEWATTS@gmail.com',
+	'gqunxjso',
+	'10/8/1928',
+	'O homem nasceu para lutar e a sua vida eh uma eterna batalha.');
+
+insert into users values(
+	'33',
+	'SETH JEFFERSON',
+	'Wenzhou',
+	'China',
+	'SETHJEFFERSON@hotmail.com',
+	'epbhvkvh',
+	'22/2/2010',
+	'O primeiro grau do heroismo eh vencer o medo.');
+
+insert into users values(
+	'34',
+	'NORMA HARRIS',
+	'Shanghai',
+	'China',
+	'NORMAHARRIS@hotmail.com',
+	'dnppzhvq',
+	'22/10/2015',
+	'Viver significa lutar.');
+
+insert into users values(
+	'35',
+	'DENNIS DIAZ',
+	'Seoul',
+	'South_Korea',
+	'DENNISDIAZ@ualg.pt',
+	'fhaiload',
+	'15/9/1920',
+	'So eh lutador quem sabe lutar consigo mesmo.');
+
+insert into users values(
+	'36',
+	'VERNA ROBERTS',
+	'Xiamen',
+	'China',
+	'VERNAROBERTS@gmail.com',
+	'igjcvjbf',
+	'24/11/1993',
+	'Sonha e seras livre de espirito... luta e seras livre na vida.');
+
+insert into users values(
+	'37',
+	'LAURA BENNETT',
+	'Ningbo',
+	'China',
+	'LAURABENNETT@ualg.pt',
+	'zdkeqzyh',
+	'19/5/2006',
+	'Nao ha dor que o sono nao consiga vencer.');
+
+insert into users values(
+	'38',
+	'GUY DRAKE',
+	'Alexandria',
+	'Egypt',
+	'GUYDRAKE@hotmail.com',
+	'bjmeaubv',
+	'26/10/1998',
+	'Viver significa lutar.');
+
+insert into users values(
+	'39',
+	'KURT PADILLA',
+	'Cape_Town',
+	'South',
+	'KURTPADILLA@gmail.com',
+	'liboegpj',
+	'18/2/1931',
+	'O mal nao pode vencer o mal. So o bem pode faze lo.');
+
+insert into users values(
+	'40',
+	'DOUGLAS SHORT',
+	'Shenyang',
+	'China',
+	'DOUGLASSHORT@hotmail.com',
+	'zkgvobnd',
+	'24/10/1931',
+	'So eh lutador quem sabe lutar consigo mesmo.');
+
+insert into users values(
+	'41',
+	'MITCHELL HOGAN',
+	'Berlin',
+	'Germany',
+	'MITCHELLHOGAN@gmail.com',
+	'hvvxpkhd',
+	'23/1/1962',
+	'A arte de vencer se aprende nas derrotas.');
+
+insert into users values(
+	'42',
+	'JERRY ROSALES',
+	'Los_Angeles',
+	'United',
+	'JERRYROSALES@ualg.pt',
+	'sytlvhmg',
+	'6/11/1937',
+	'Nao ha dor que o sono nao consiga vencer.');
+
+insert into users values(
+	'43',
+	'HERMAN COLLINS',
+	'Cairo',
+	'Egypt',
+	'HERMANCOLLINS@gmail.com',
+	'tbaxcgot',
+	'14/3/1993',
+	'Prefiro perder a guerra e ganhar a paz.');
+
+insert into users values(
+	'44',
+	'SHELLY GAINES',
+	'Johannesburg',
+	'South_Africa',
+	'SHELLYGAINES@hotmail.com',
+	'blpmzkej',
+	'12/8/1933',
+	'Nada eh mais humilhante do que ver os tolos vencer naquilo em que fracassamos.');
+
+insert into users values(
+	'45',
+	'MANUEL KRAMER',
+	'Beijing',
+	'China',
+	'MANUELKRAMER@gmail.com',
+	'xgsmmmtt',
+	'15/8/1998',
+	'So se pode vencer a natureza obedecendo lhe.');
+
+insert into users values(
+	'46',
+	'JERRY CRANE',
+	'Dhaka',
+	'Bangladesh',
+	'JERRYCRANE@outlook.com',
+	'dmyqemec',
+	'22/9/1991',
+	'Eh mais facil vencer um mau habito hoje do que amanha.');
+
+insert into users values(
+	'47',
+	'DUSTIN FRY',
+	'Hefei',
+	'China',
+	'DUSTINFRY@hotmail.com',
+	'msohbchc',
+	'11/9/1945',
+	'Prefiro perder a guerra e ganhar a paz.');
+
+insert into users values(
+	'48',
+	'TRAVIS JACKSON',
+	'Kinshasa',
+	'Democratic_Republic_of_the_Congo',
+	'TRAVISJACKSON@gmail.com',
+	'nohljkac',
+	'18/9/1986',
+	'A arte de vencer se aprende nas derrotas.');
+
+insert into users values(
+	'49',
+	'HELEN ALLISON',
+	'Hanoi',
+	'Vietnam',
+	'HELENALLISON@ualg.pt',
+	'ivxtbrcn',
+	'16/8/1935',
+	'Eh mais facil vencer um mau habito hoje do que amanha.');
+
