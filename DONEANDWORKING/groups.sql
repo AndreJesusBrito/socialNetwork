e4781d916fa572bc86c330804ef66d904c4fec8e
cptSagres <39467476+cptSagres@users.noreply.github.com>
2018-11-26 19:45:01 +0000

    BIG CHANGES YEAY

---
+++ DONEANDWORKING/groups.sql
@@ -0,0 +1,1408 @@
+insert into groups values(
+	'0',
+	'Crime',
+	'empty',
+	TRUE,
+	FALSE,
+	'3');
+
+insert into groups values(
+	'1',
+	'Locations',
+	'empty',
+	FALSE,
+	TRUE,
+	'18');
+
+insert into groups values(
+	'2',
+	'Perfection',
+	'empty',
+	TRUE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'3',
+	'Peace',
+	'empty',
+	FALSE,
+	TRUE,
+	'16');
+
+insert into groups values(
+	'4',
+	'Change',
+	'empty',
+	FALSE,
+	TRUE,
+	'8');
+
+insert into groups values(
+	'5',
+	'Photography',
+	'empty',
+	TRUE,
+	TRUE,
+	'17');
+
+insert into groups values(
+	'6',
+	'Boats',
+	'empty',
+	TRUE,
+	FALSE,
+	'6');
+
+insert into groups values(
+	'7',
+	'Treason',
+	'empty',
+	FALSE,
+	FALSE,
+	'2');
+
+insert into groups values(
+	'8',
+	'Vices',
+	'empty',
+	TRUE,
+	TRUE,
+	'15');
+
+insert into groups values(
+	'9',
+	'Transport',
+	'empty',
+	TRUE,
+	FALSE,
+	'18');
+
+insert into groups values(
+	'10',
+	'Loyalty',
+	'empty',
+	FALSE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'11',
+	'Simplicity',
+	'empty',
+	TRUE,
+	FALSE,
+	'13');
+
+insert into groups values(
+	'12',
+	'Work',
+	'empty',
+	FALSE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'13',
+	'Suicide',
+	'empty',
+	TRUE,
+	TRUE,
+	'12');
+
+insert into groups values(
+	'14',
+	'Comics',
+	'empty',
+	FALSE,
+	FALSE,
+	'0');
+
+insert into groups values(
+	'15',
+	'Essentialism',
+	'empty',
+	FALSE,
+	TRUE,
+	'15');
+
+insert into groups values(
+	'16',
+	'Sunrise / Sunset',
+	'empty',
+	TRUE,
+	TRUE,
+	'2');
+
+insert into groups values(
+	'17',
+	'Sound',
+	'empty',
+	TRUE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'18',
+	'Fools',
+	'empty',
+	FALSE,
+	TRUE,
+	'1');
+
+insert into groups values(
+	'19',
+	'Freedom',
+	'empty',
+	FALSE,
+	TRUE,
+	'10');
+
+insert into groups values(
+	'20',
+	'Relationships',
+	'empty',
+	FALSE,
+	TRUE,
+	'18');
+
+insert into groups values(
+	'21',
+	'Recording',
+	'empty',
+	FALSE,
+	FALSE,
+	'12');
+
+insert into groups values(
+	'22',
+	'Ecology',
+	'empty',
+	TRUE,
+	FALSE,
+	'14');
+
+insert into groups values(
+	'23',
+	'Forethought',
+	'empty',
+	TRUE,
+	TRUE,
+	'13');
+
+insert into groups values(
+	'24',
+	'Boredom',
+	'empty',
+	TRUE,
+	TRUE,
+	'18');
+
+insert into groups values(
+	'25',
+	'Creation and evolution',
+	'empty',
+	FALSE,
+	TRUE,
+	'19');
+
+insert into groups values(
+	'26',
+	'Hell',
+	'empty',
+	TRUE,
+	FALSE,
+	'16');
+
+insert into groups values(
+	'27',
+	'Ideas',
+	'empty',
+	FALSE,
+	FALSE,
+	'15');
+
+insert into groups values(
+	'28',
+	'Propaganda',
+	'empty',
+	FALSE,
+	TRUE,
+	'19');
+
+insert into groups values(
+	'29',
+	'Humor',
+	'empty',
+	FALSE,
+	TRUE,
+	'9');
+
+insert into groups values(
+	'30',
+	'New Zealand',
+	'empty',
+	TRUE,
+	TRUE,
+	'10');
+
+insert into groups values(
+	'31',
+	'Failure',
+	'empty',
+	FALSE,
+	TRUE,
+	'2');
+
+insert into groups values(
+	'32',
+	'Politics',
+	'empty',
+	TRUE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'33',
+	'Future',
+	'empty',
+	TRUE,
+	FALSE,
+	'3');
+
+insert into groups values(
+	'34',
+	'Public Speaking',
+	'empty',
+	TRUE,
+	FALSE,
+	'6');
+
+insert into groups values(
+	'35',
+	'Philosophy',
+	'empty',
+	TRUE,
+	FALSE,
+	'12');
+
+insert into groups values(
+	'36',
+	'Transgender',
+	'empty',
+	FALSE,
+	FALSE,
+	'12');
+
+insert into groups values(
+	'37',
+	'Acquisition',
+	'empty',
+	TRUE,
+	TRUE,
+	'6');
+
+insert into groups values(
+	'38',
+	'Courage',
+	'empty',
+	TRUE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'39',
+	'Truth',
+	'empty',
+	TRUE,
+	TRUE,
+	'0');
+
+insert into groups values(
+	'40',
+	'Marriage',
+	'empty',
+	TRUE,
+	TRUE,
+	'2');
+
+insert into groups values(
+	'41',
+	'Libertarianism',
+	'empty',
+	FALSE,
+	FALSE,
+	'2');
+
+insert into groups values(
+	'42',
+	'Spring',
+	'empty',
+	FALSE,
+	TRUE,
+	'7');
+
+insert into groups values(
+	'43',
+	'Upset',
+	'empty',
+	TRUE,
+	FALSE,
+	'17');
+
+insert into groups values(
+	'44',
+	'Mathematics',
+	'empty',
+	FALSE,
+	TRUE,
+	'6');
+
+insert into groups values(
+	'45',
+	'Fascism',
+	'empty',
+	TRUE,
+	FALSE,
+	'16');
+
+insert into groups values(
+	'46',
+	'Tea',
+	'empty',
+	TRUE,
+	FALSE,
+	'14');
+
+insert into groups values(
+	'47',
+	'Reality',
+	'empty',
+	FALSE,
+	TRUE,
+	'4');
+
+insert into groups values(
+	'48',
+	'Physics',
+	'empty',
+	FALSE,
+	TRUE,
+	'10');
+
+insert into groups values(
+	'49',
+	'Happiness',
+	'empty',
+	FALSE,
+	FALSE,
+	'16');
+
+insert into groups values(
+	'50',
+	'Serialism',
+	'empty',
+	FALSE,
+	FALSE,
+	'16');
+
+insert into groups values(
+	'51',
+	'Bokononism',
+	'empty',
+	TRUE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'52',
+	'Sports',
+	'empty',
+	TRUE,
+	TRUE,
+	'16');
+
+insert into groups values(
+	'53',
+	'Gay, lesbian, bisexual, and transgender',
+	'empty',
+	TRUE,
+	FALSE,
+	'11');
+
+insert into groups values(
+	'54',
+	'Tonality',
+	'empty',
+	FALSE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'55',
+	'Democracy',
+	'empty',
+	FALSE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'56',
+	'Conspiracy',
+	'empty',
+	FALSE,
+	FALSE,
+	'3');
+
+insert into groups values(
+	'57',
+	'Vegetarianism',
+	'empty',
+	FALSE,
+	FALSE,
+	'17');
+
+insert into groups values(
+	'58',
+	'Absurdity',
+	'empty',
+	FALSE,
+	FALSE,
+	'8');
+
+insert into groups values(
+	'59',
+	'Atheism',
+	'empty',
+	FALSE,
+	FALSE,
+	'17');
+
+insert into groups values(
+	'60',
+	'Beauty',
+	'empty',
+	FALSE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'61',
+	'Technology',
+	'empty',
+	FALSE,
+	FALSE,
+	'11');
+
+insert into groups values(
+	'62',
+	'Sadism and Masochism',
+	'empty',
+	FALSE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'63',
+	'Masturbation',
+	'empty',
+	TRUE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'64',
+	'Anger',
+	'empty',
+	TRUE,
+	TRUE,
+	'11');
+
+insert into groups values(
+	'65',
+	'Patience',
+	'empty',
+	TRUE,
+	TRUE,
+	'13');
+
+insert into groups values(
+	'66',
+	'Citizenship',
+	'empty',
+	FALSE,
+	TRUE,
+	'11');
+
+insert into groups values(
+	'67',
+	'Middle name',
+	'empty',
+	TRUE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'68',
+	'Film',
+	'empty',
+	FALSE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'69',
+	'Religion',
+	'empty',
+	FALSE,
+	TRUE,
+	'19');
+
+insert into groups values(
+	'70',
+	'Valor',
+	'empty',
+	TRUE,
+	FALSE,
+	'17');
+
+insert into groups values(
+	'71',
+	'Elders',
+	'empty',
+	FALSE,
+	TRUE,
+	'18');
+
+insert into groups values(
+	'72',
+	'Bias',
+	'empty',
+	FALSE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'73',
+	'Bravery',
+	'empty',
+	TRUE,
+	TRUE,
+	'7');
+
+insert into groups values(
+	'74',
+	'Journalism',
+	'empty',
+	TRUE,
+	TRUE,
+	'1');
+
+insert into groups values(
+	'75',
+	'Debate',
+	'empty',
+	TRUE,
+	FALSE,
+	'5');
+
+insert into groups values(
+	'76',
+	'Dreams',
+	'empty',
+	TRUE,
+	FALSE,
+	'0');
+
+insert into groups values(
+	'77',
+	'Mercy',
+	'empty',
+	TRUE,
+	TRUE,
+	'12');
+
+insert into groups values(
+	'78',
+	'Gender',
+	'empty',
+	TRUE,
+	FALSE,
+	'0');
+
+insert into groups values(
+	'79',
+	'Rainbows',
+	'empty',
+	TRUE,
+	TRUE,
+	'1');
+
+insert into groups values(
+	'80',
+	'Information',
+	'empty',
+	FALSE,
+	FALSE,
+	'2');
+
+insert into groups values(
+	'81',
+	'God',
+	'empty',
+	TRUE,
+	FALSE,
+	'16');
+
+insert into groups values(
+	'82',
+	'Extinction',
+	'empty',
+	FALSE,
+	FALSE,
+	'11');
+
+insert into groups values(
+	'83',
+	'Curses',
+	'empty',
+	FALSE,
+	FALSE,
+	'4');
+
+insert into groups values(
+	'84',
+	'Protection',
+	'empty',
+	FALSE,
+	TRUE,
+	'11');
+
+insert into groups values(
+	'85',
+	'Concert',
+	'empty',
+	FALSE,
+	TRUE,
+	'18');
+
+insert into groups values(
+	'86',
+	'Wealth',
+	'empty',
+	TRUE,
+	TRUE,
+	'17');
+
+insert into groups values(
+	'87',
+	'Swearing',
+	'empty',
+	FALSE,
+	FALSE,
+	'18');
+
+insert into groups values(
+	'88',
+	'Solitude',
+	'empty',
+	FALSE,
+	TRUE,
+	'13');
+
+insert into groups values(
+	'89',
+	'Government',
+	'empty',
+	TRUE,
+	TRUE,
+	'0');
+
+insert into groups values(
+	'90',
+	'Chess',
+	'empty',
+	FALSE,
+	TRUE,
+	'8');
+
+insert into groups values(
+	'91',
+	'Success',
+	'empty',
+	FALSE,
+	TRUE,
+	'11');
+
+insert into groups values(
+	'92',
+	'Belief',
+	'empty',
+	FALSE,
+	FALSE,
+	'11');
+
+insert into groups values(
+	'93',
+	'Scratching',
+	'empty',
+	FALSE,
+	FALSE,
+	'17');
+
+insert into groups values(
+	'94',
+	'Drinking',
+	'empty',
+	TRUE,
+	TRUE,
+	'11');
+
+insert into groups values(
+	'95',
+	'Honor',
+	'empty',
+	FALSE,
+	FALSE,
+	'8');
+
+insert into groups values(
+	'96',
+	'Monarchy',
+	'empty',
+	TRUE,
+	FALSE,
+	'14');
+
+insert into groups values(
+	'97',
+	'Identity',
+	'empty',
+	FALSE,
+	TRUE,
+	'11');
+
+insert into groups values(
+	'98',
+	'Project Management',
+	'empty',
+	FALSE,
+	TRUE,
+	'16');
+
+insert into groups values(
+	'99',
+	'Terrorism',
+	'empty',
+	FALSE,
+	TRUE,
+	'2');
+
+insert into groups values(
+	'100',
+	'Pollution',
+	'empty',
+	TRUE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'101',
+	'Zoology',
+	'empty',
+	TRUE,
+	TRUE,
+	'9');
+
+insert into groups values(
+	'102',
+	'Parents',
+	'empty',
+	TRUE,
+	TRUE,
+	'7');
+
+insert into groups values(
+	'103',
+	'Abstinence',
+	'empty',
+	FALSE,
+	TRUE,
+	'13');
+
+insert into groups values(
+	'104',
+	'Aviation',
+	'empty',
+	TRUE,
+	TRUE,
+	'1');
+
+insert into groups values(
+	'105',
+	'Wisdom',
+	'empty',
+	FALSE,
+	TRUE,
+	'16');
+
+insert into groups values(
+	'106',
+	'Jealousy',
+	'empty',
+	TRUE,
+	TRUE,
+	'12');
+
+insert into groups values(
+	'107',
+	'Ghosts',
+	'empty',
+	FALSE,
+	FALSE,
+	'5');
+
+insert into groups values(
+	'108',
+	'Conservation',
+	'empty',
+	FALSE,
+	FALSE,
+	'15');
+
+insert into groups values(
+	'109',
+	'Loneliness',
+	'empty',
+	TRUE,
+	FALSE,
+	'5');
+
+insert into groups values(
+	'110',
+	'Revolution',
+	'empty',
+	FALSE,
+	TRUE,
+	'16');
+
+insert into groups values(
+	'111',
+	'Obsession',
+	'empty',
+	FALSE,
+	FALSE,
+	'19');
+
+insert into groups values(
+	'112',
+	'Plants',
+	'empty',
+	FALSE,
+	FALSE,
+	'7');
+
+insert into groups values(
+	'113',
+	'Mankind',
+	'empty',
+	FALSE,
+	FALSE,
+	'12');
+
+insert into groups values(
+	'114',
+	'Fall (Autumn)',
+	'empty',
+	FALSE,
+	FALSE,
+	'3');
+
+insert into groups values(
+	'115',
+	'Literature',
+	'empty',
+	FALSE,
+	FALSE,
+	'0');
+
+insert into groups values(
+	'116',
+	'Secrecy',
+	'empty',
+	TRUE,
+	TRUE,
+	'15');
+
+insert into groups values(
+	'117',
+	'Betrayal',
+	'empty',
+	TRUE,
+	FALSE,
+	'6');
+
+insert into groups values(
+	'118',
+	'Isolation',
+	'empty',
+	FALSE,
+	FALSE,
+	'2');
+
+insert into groups values(
+	'119',
+	'Communism',
+	'empty',
+	TRUE,
+	FALSE,
+	'7');
+
+insert into groups values(
+	'120',
+	'Necessity',
+	'empty',
+	FALSE,
+	FALSE,
+	'19');
+
+insert into groups values(
+	'121',
+	'Oil',
+	'empty',
+	TRUE,
+	TRUE,
+	'19');
+
+insert into groups values(
+	'122',
+	'Slavery',
+	'empty',
+	TRUE,
+	TRUE,
+	'13');
+
+insert into groups values(
+	'123',
+	'Tolerance',
+	'empty',
+	FALSE,
+	FALSE,
+	'14');
+
+insert into groups values(
+	'124',
+	'Summer',
+	'empty',
+	TRUE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'125',
+	'Sarcasm',
+	'empty',
+	FALSE,
+	TRUE,
+	'4');
+
+insert into groups values(
+	'126',
+	'Writing',
+	'empty',
+	TRUE,
+	TRUE,
+	'9');
+
+insert into groups values(
+	'127',
+	'Despair',
+	'empty',
+	TRUE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'128',
+	'Trust',
+	'empty',
+	TRUE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'129',
+	'Smuggling',
+	'empty',
+	TRUE,
+	TRUE,
+	'9');
+
+insert into groups values(
+	'130',
+	'Teams and team work',
+	'empty',
+	FALSE,
+	TRUE,
+	'14');
+
+insert into groups values(
+	'131',
+	'Video Games',
+	'empty',
+	FALSE,
+	TRUE,
+	'12');
+
+insert into groups values(
+	'132',
+	'Sociology',
+	'empty',
+	FALSE,
+	FALSE,
+	'4');
+
+insert into groups values(
+	'133',
+	'Good',
+	'empty',
+	TRUE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'134',
+	'Segregation',
+	'empty',
+	TRUE,
+	FALSE,
+	'5');
+
+insert into groups values(
+	'135',
+	'Art',
+	'empty',
+	TRUE,
+	FALSE,
+	'16');
+
+insert into groups values(
+	'136',
+	'Time',
+	'empty',
+	FALSE,
+	FALSE,
+	'6');
+
+insert into groups values(
+	'137',
+	'Accusation',
+	'empty',
+	TRUE,
+	TRUE,
+	'6');
+
+insert into groups values(
+	'138',
+	'Socialism',
+	'empty',
+	TRUE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'139',
+	'Brothers',
+	'empty',
+	FALSE,
+	TRUE,
+	'10');
+
+insert into groups values(
+	'140',
+	'Immortality',
+	'empty',
+	FALSE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'141',
+	'Celebratory',
+	'empty',
+	TRUE,
+	FALSE,
+	'6');
+
+insert into groups values(
+	'142',
+	'Canada',
+	'empty',
+	TRUE,
+	TRUE,
+	'19');
+
+insert into groups values(
+	'143',
+	'Self discipline',
+	'empty',
+	FALSE,
+	FALSE,
+	'5');
+
+insert into groups values(
+	'144',
+	'Trees',
+	'empty',
+	FALSE,
+	FALSE,
+	'12');
+
+insert into groups values(
+	'145',
+	'Golf',
+	'empty',
+	FALSE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'146',
+	'Opinion',
+	'empty',
+	TRUE,
+	FALSE,
+	'7');
+
+insert into groups values(
+	'147',
+	'Abortion',
+	'empty',
+	TRUE,
+	TRUE,
+	'0');
+
+insert into groups values(
+	'148',
+	'Ability',
+	'empty',
+	FALSE,
+	FALSE,
+	'15');
+
+insert into groups values(
+	'149',
+	'Language',
+	'empty',
+	FALSE,
+	FALSE,
+	'14');
+
+insert into groups values(
+	'150',
+	'Homosexuality',
+	'empty',
+	TRUE,
+	FALSE,
+	'18');
+
+insert into groups values(
+	'151',
+	'Ideology',
+	'empty',
+	FALSE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'152',
+	'Form',
+	'empty',
+	TRUE,
+	FALSE,
+	'5');
+
+insert into groups values(
+	'153',
+	'Computers',
+	'empty',
+	FALSE,
+	FALSE,
+	'19');
+
+insert into groups values(
+	'154',
+	'Christmas',
+	'empty',
+	FALSE,
+	TRUE,
+	'2');
+
+insert into groups values(
+	'155',
+	'Automobiles',
+	'empty',
+	FALSE,
+	TRUE,
+	'17');
+
+insert into groups values(
+	'156',
+	'Mass culture',
+	'empty',
+	TRUE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'157',
+	'Mothers',
+	'empty',
+	FALSE,
+	FALSE,
+	'7');
+
+insert into groups values(
+	'158',
+	'Health',
+	'empty',
+	TRUE,
+	TRUE,
+	'15');
+
+insert into groups values(
+	'159',
+	'Linguistics',
+	'empty',
+	TRUE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'160',
+	'Disability',
+	'empty',
+	TRUE,
+	FALSE,
+	'1');
+
+insert into groups values(
+	'161',
+	'Apathy',
+	'empty',
+	TRUE,
+	TRUE,
+	'16');
+
+insert into groups values(
+	'162',
+	'Faith',
+	'empty',
+	FALSE,
+	TRUE,
+	'17');
+
+insert into groups values(
+	'163',
+	'Form',
+	'empty',
+	FALSE,
+	TRUE,
+	'2');
+
+insert into groups values(
+	'164',
+	'Coffee',
+	'empty',
+	FALSE,
+	TRUE,
+	'3');
+
+insert into groups values(
+	'165',
+	'Boys',
+	'empty',
+	FALSE,
+	TRUE,
+	'5');
+
+insert into groups values(
+	'166',
+	'Age',
+	'empty',
+	TRUE,
+	FALSE,
+	'17');
+
+insert into groups values(
+	'167',
+	'Fire',
+	'empty',
+	TRUE,
+	FALSE,
+	'8');
+
+insert into groups values(
+	'168',
+	'Anthropology',
+	'empty',
+	TRUE,
+	FALSE,
+	'6');
+
+insert into groups values(
+	'169',
+	'Knowledge',
+	'empty',
+	TRUE,
+	TRUE,
+	'12');
+
+insert into groups values(
+	'170',
+	'Pride',
+	'empty',
+	TRUE,
+	TRUE,
+	'7');
+
+insert into groups values(
+	'171',
+	'Tonality',
+	'empty',
+	TRUE,
+	FALSE,
+	'15');
+
+insert into groups values(
+	'172',
+	'Independency',
+	'empty',
+	TRUE,
+	FALSE,
+	'9');
+
+insert into groups values(
+	'173',
+	'Profit',
+	'empty',
+	TRUE,
+	FALSE,
+	'4');
+
+insert into groups values(
+	'174',
+	'Television',
+	'empty',
+	TRUE,
+	TRUE,
+	'19');
+
+insert into groups values(
+	'175',
+	'Conservation',
+	'empty',
+	FALSE,
+	FAlSE,
+	'3');
+	
