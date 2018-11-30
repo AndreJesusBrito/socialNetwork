bcaf901b28a22d9f937ef4017372483bad85c1f1
cptSagres <39467476+cptSagres@users.noreply.github.com>
2018-11-30 14:35:55 +0000

    Update role_script.js

--- PROGRAMS THAT I USED/role_script.js
+++ PROGRAMS THAT I USED/role_script.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 
 
 //generate post 
-var sub,color,n=9,sr='';
+var color,n=9,sr='';
+var role = ['apprentice wizard','sensei','el duche','shogun','ebony chieftain','chad mage','legendary sandro','negod'];
+
 for(var i = 0; i < 350;i++){
-	for(var j = 0; j < 9; j++){
-	color = faker.internet.color();
-	sr+=`insert into role values(
+	for(var j = 0; j < 8;j++){
+		color = faker.internet.color();
+		sr+=`insert into role values(
 		\t'${i}\',
-		\t'${j}\',
+		\t'${role[j]}\',
 		\t'${j}\',
 		\t'${color}');
 		\r\n\r\n`;
 	}
-
 }
-fs.writeFile('C:/Users/Bruno/Documents/GitHub/socialNetwork/PROGRAMS THAT I USED/role_info.sql', sr,function(err){});
+fs.writeFile('C:/Users/Bruno/Documents/JS/role.sql', sr,function(err){});
