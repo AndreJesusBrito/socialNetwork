dcd46633ba287ef9bccdc281a8d05328eba5a0bf
cptSagres <39467476+cptSagres@users.noreply.github.com>
2018-11-30 18:41:08 +0000

    COMMIT 2

---
+++ DONEANDWORKING/500users/friends.sql
@@ -0,0 +1,685 @@
+insert into friend values (
+	'217',
+	'164'
+);
+
+insert into friend values (
+	'26',
+	'228'
+);
+
+insert into friend values (
+	'206',
+	'204'
+);
+
+insert into friend values (
+	'71',
+	'196'
+);
+
+insert into friend values (
+	'8',
+	'119'
+);
+
+insert into friend values (
+	'151',
+	'93'
+);
+
+insert into friend values (
+	'157',
+	'184'
+);
+
+insert into friend values (
+	'69',
+	'201'
+);
+
+insert into friend values (
+	'202',
+	'99'
+);
+
+insert into friend values (
+	'69',
+	'192'
+);
+
+insert into friend values (
+	'23',
+	'89'
+);
+
+insert into friend values (
+	'189',
+	'223'
+);
+
+insert into friend values (
+	'81',
+	'40'
+);
+
+insert into friend values (
+	'75',
+	'166'
+);
+
+insert into friend values (
+	'23',
+	'203'
+);
+
+insert into friend values (
+	'245',
+	'138'
+);
+
+insert into friend values (
+	'107',
+	'47'
+);
+
+insert into friend values (
+	'122',
+	'111'
+);
+
+insert into friend values (
+	'176',
+	'188'
+);
+
+insert into friend values (
+	'25',
+	'198'
+);
+
+insert into friend values (
+	'2',
+	'63'
+);
+
+insert into friend values (
+	'236',
+	'227'
+);
+
+insert into friend values (
+	'21',
+	'229'
+);
+
+insert into friend values (
+	'217',
+	'227'
+);
+
+insert into friend values (
+	'30',
+	'35'
+);
+
+insert into friend values (
+	'100',
+	'56'
+);
+
+insert into friend values (
+	'110',
+	'175'
+);
+
+insert into friend values (
+	'222',
+	'188'
+);
+
+insert into friend values (
+	'171',
+	'237'
+);
+
+insert into friend values (
+	'62',
+	'200'
+);
+
+insert into friend values (
+	'131',
+	'55'
+);
+
+insert into friend values (
+	'155',
+	'223'
+);
+
+insert into friend values (
+	'136',
+	'188'
+);
+
+insert into friend values (
+	'124',
+	'215'
+);
+
+insert into friend values (
+	'109',
+	'83'
+);
+
+insert into friend values (
+	'183',
+	'97'
+);
+
+insert into friend values (
+	'209',
+	'66'
+);
+
+insert into friend values (
+	'102',
+	'78'
+);
+
+insert into friend values (
+	'86',
+	'139'
+);
+
+insert into friend values (
+	'47',
+	'86'
+);
+
+insert into friend values (
+	'128',
+	'115'
+);
+
+insert into friend values (
+	'232',
+	'88'
+);
+
+insert into friend values (
+	'194',
+	'157'
+);
+
+insert into friend values (
+	'161',
+	'141'
+);
+
+insert into friend values (
+	'217',
+	'13'
+);
+
+insert into friend values (
+	'137',
+	'216'
+);
+
+insert into friend values (
+	'68',
+	'38'
+);
+
+insert into friend values (
+	'99',
+	'94'
+);
+
+insert into friend values (
+	'76',
+	'37'
+);
+
+insert into friend values (
+	'83',
+	'2'
+);
+
+insert into friend values (
+	'193',
+	'187'
+);
+
+insert into friend values (
+	'197',
+	'172'
+);
+
+insert into friend values (
+	'225',
+	'6'
+);
+
+insert into friend values (
+	'77',
+	'68'
+);
+
+insert into friend values (
+	'31',
+	'89'
+);
+
+insert into friend values (
+	'35',
+	'15'
+);
+
+insert into friend values (
+	'132',
+	'96'
+);
+
+insert into friend values (
+	'92',
+	'106'
+);
+
+insert into friend values (
+	'102',
+	'193'
+);
+
+insert into friend values (
+	'143',
+	'189'
+);
+
+insert into friend values (
+	'177',
+	'81'
+);
+
+insert into friend values (
+	'213',
+	'33'
+);
+
+insert into friend values (
+	'165',
+	'151'
+);
+
+insert into friend values (
+	'111',
+	'144'
+);
+
+insert into friend values (
+	'57',
+	'247'
+);
+
+insert into friend values (
+	'112',
+	'140'
+);
+
+insert into friend values (
+	'199',
+	'92'
+);
+
+insert into friend values (
+	'228',
+	'182'
+);
+
+insert into friend values (
+	'115',
+	'97'
+);
+
+insert into friend values (
+	'219',
+	'59'
+);
+
+insert into friend values (
+	'112',
+	'33'
+);
+
+insert into friend values (
+	'192',
+	'207'
+);
+
+insert into friend values (
+	'165',
+	'130'
+);
+
+insert into friend values (
+	'106',
+	'123'
+);
+
+insert into friend values (
+	'0',
+	'30'
+);
+
+insert into friend values (
+	'97',
+	'159'
+);
+
+insert into friend values (
+	'51',
+	'236'
+);
+
+insert into friend values (
+	'17',
+	'246'
+);
+
+insert into friend values (
+	'14',
+	'189'
+);
+
+insert into friend values (
+	'101',
+	'205'
+);
+
+insert into friend values (
+	'197',
+	'193'
+);
+
+insert into friend values (
+	'193',
+	'29'
+);
+
+insert into friend values (
+	'225',
+	'168'
+);
+
+insert into friend values (
+	'172',
+	'148'
+);
+
+insert into friend values (
+	'154',
+	'106'
+);
+
+insert into friend values (
+	'58',
+	'20'
+);
+
+insert into friend values (
+	'111',
+	'74'
+);
+
+insert into friend values (
+	'52',
+	'23'
+);
+
+insert into friend values (
+	'213',
+	'58'
+);
+
+insert into friend values (
+	'74',
+	'60'
+);
+
+insert into friend values (
+	'48',
+	'2'
+);
+
+insert into friend values (
+	'249',
+	'203'
+);
+
+insert into friend values (
+	'156',
+	'224'
+);
+
+insert into friend values (
+	'173',
+	'38'
+);
+
+insert into friend values (
+	'204',
+	'58'
+);
+
+insert into friend values (
+	'92',
+	'42'
+);
+
+insert into friend values (
+	'140',
+	'176'
+);
+
+insert into friend values (
+	'62',
+	'111'
+);
+
+insert into friend values (
+	'236',
+	'2'
+);
+
+insert into friend values (
+	'168',
+	'119'
+);
+
+insert into friend values (
+	'82',
+	'102'
+);
+
+insert into friend values (
+	'40',
+	'108'
+);
+
+insert into friend values (
+	'163',
+	'196'
+);
+
+insert into friend values (
+	'115',
+	'243'
+);
+
+insert into friend values (
+	'105',
+	'137'
+);
+
+insert into friend values (
+	'121',
+	'187'
+);
+
+insert into friend values (
+	'234',
+	'12'
+);
+
+insert into friend values (
+	'172',
+	'128'
+);
+
+insert into friend values (
+	'30',
+	'193'
+);
+
+insert into friend values (
+	'206',
+	'138'
+);
+
+insert into friend values (
+	'242',
+	'1'
+);
+
+insert into friend values (
+	'236',
+	'21'
+);
+
+insert into friend values (
+	'195',
+	'230'
+);
+
+insert into friend values (
+	'49',
+	'39'
+);
+
+insert into friend values (
+	'11',
+	'4'
+);
+
+insert into friend values (
+	'220',
+	'110'
+);
+
+insert into friend values (
+	'49',
+	'109'
+);
+
+insert into friend values (
+	'86',
+	'184'
+);
+
+insert into friend values (
+	'105',
+	'76'
+);
+
+insert into friend values (
+	'245',
+	'10'
+);
+
+insert into friend values (
+	'232',
+	'90'
+);
+
+insert into friend values (
+	'212',
+	'57'
+);
+
+insert into friend values (
+	'93',
+	'54'
+);
+
+insert into friend values (
+	'186',
+	'219'
+);
+
+insert into friend values (
+	'185',
+	'158'
+);
+
+insert into friend values (
+	'229',
+	'175'
+);
+
+insert into friend values (
+	'13',
+	'61'
+);
+
+insert into friend values (
+	'167',
+	'54'
+);
+
+insert into friend values (
+	'109',
+	'29'
+);
+
+insert into friend values (
+	'128',
+	'59'
+);
+
+insert into friend values (
+	'195',
+	'68'
+);
+
+insert into friend values (
+	'225',
+	'33'
+);
+
+insert into friend values (
+	'163',
+	'108'
+);
+
+insert into friend values (
+	'204',
+	'40'
+);
+
+insert into friend values (
+	'227',
+	'56'
+);
+
+insert into friend values (
+	'210',
+	'136'
+);
+
+insert into friend values (
+	'110',
+	'199'
+);
+
